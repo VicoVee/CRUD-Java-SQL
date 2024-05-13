7a546a9f700298a667a82a0c0f92566eaa56863d
Victoria <tovictoriadao@gmail.com>
2024-05-12 23:02:10 -0400

    Attempting to insert users from a CSV file manually?

--- Create_DB.java
+++ Create_DB.java
@@ -3,57 +3,106 @@ import java.sql.DriverManager; //Connects Java to MySQL
 import java.sql.PreparedStatement;
 import java.sql.SQLException; //Catch Errors
 import java.sql.Statement; //create statements with ease
-
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
 
 //Creating an Insert Statement in Java
 public class Create_DB {
-    static String dbURL = "jdbc:mysql://localhost:3306/sampledb"; //Get Connection Address
-    static String username = "root"; //Get the user
-    static String password = "916192991"; //Get the password
-    static String[] arr = {"MarioBros", "nintendo64", "Mario Luigi", "nintendo@gmail.com"};
+    static String dbURL = "jdbc:mysql://localhost:3306/sampledb"; // Get Connection Address
+    static String username = "root"; // Get the user
+    static String password = "916192991"; // Get the password
+    // static String[] arr = {"MarioBros", "nintendo64", "Mario Luigi",
+    // "nintendo@gmail.com"};
+    private final String COMMA_DELIMITER = ","; //Identify the columns in the CSV
 
-    public static void main(String[] args) {
-        //Create a Connection object. This will allow us to connect to the DB
-        //After everything under the try is completed, the connection will automatically close
-        try (Connection conn = DriverManager.getConnection(dbURL, username, password)) {
-            //First, write the insert statement in SQL
-            //Then convert to String and place in a Java String Object
-            //Note: Need to define the parameters (username, pass, etc) because the primary key
-            //is already auto_incremented? Will check on this later
-            String userBob = "INSERT INTO Users (username, password, fullname, email) VALUES(?, ?, ?, ?)";
+    public void addUsers(BufferedReader br) {
+        //Holds all the parameters for the SQL statement
+        List<String> userInfo = new ArrayList<>();
 
-            //Insert each parameter manually using prepareStatement
-            PreparedStatement insertBob = conn.prepareStatement(userBob);
-            //Since each parameter is a varchar/string, use the setString method
-            insertBob.setString(1, "BobLovesMe");
-            insertBob.setString(2, "password134");
-            insertBob.setString(3, "Bob Larry");
-            insertBob.setString(4, "lovemenots@yahoo.com");
+        try {
+            String line;
+            br.readLine(); //Skips the column names 
 
-            //executeUpdate() runs the SQL command and sends to database
-            //Returns the number of rows returned or 0 if nothing is returned
-            int rowInserted = insertBob.executeUpdate();
-            if(rowInserted > 0){
-                System.out.println("A user has been inserted");
+            //While the line is NOT NULL, read given columns to add to parameters
+            while ((line = br.readLine()) != null) {
+                //Split lines to appropiate columns
+                String[] temp = line.split(COMMA_DELIMITER);
+                
+                for(String i : temp) {
+                    System.out.println(i);
+                }
             }
-
-            //Testing something else
-            String userMarie = "INSERT INTO Users (username, password, fullname, email) VALUES (?, ?, ?, ?)";
-
-            PreparedStatement insertMarie = conn.prepareStatement(userMarie);
-
-            for(int i = 0; i < arr.length; i++){
-                insertMarie.setString(i+1, arr[i]);
-            }
-            rowInserted = insertMarie.executeUpdate();
-            if(rowInserted > 0){
-                System.out.println("Marie has been added as a user!");
-            }
-            else {
-                System.out.println("Insertion Failed");
-            }
-        } catch (SQLException ex) {
-            ex.printStackTrace();
+        } catch (IOException ioe) {
+            System.out.println("Oop something went wrong and I have no clue (file input error)");
+            ioe.printStackTrace();
+            System.exit(1);
         }
+
     }
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // public static void main(String[] args) {
+    // //Create a Connection object. This will allow us to connect to the DB
+    // //After everything under the try is completed, the connection will
+    // automatically close
+    // try (Connection conn = DriverManager.getConnection(dbURL, username,
+    // password)) {
+    // //First, write the insert statement in SQL
+    // //Then convert to String and place in a Java String Object
+    // //Note: Need to define the parameters (username, pass, etc) because the
+    // primary key
+    // //is already auto_incremented? Will check on this later
+    // String userBob = "INSERT INTO Users (username, password, fullname, email)
+    // VALUES(?, ?, ?, ?)";
+
+    // //Insert each parameter manually using prepareStatement
+    // PreparedStatement insertBob = conn.prepareStatement(userBob);
+    // //Since each parameter is a varchar/string, use the setString method
+    // insertBob.setString(1, "BobLovesMe");
+    // insertBob.setString(2, "password134");
+    // insertBob.setString(3, "Bob Larry");
+    // insertBob.setString(4, "lovemenots@yahoo.com");
+
+    // //executeUpdate() runs the SQL command and sends to database
+    // //Returns the number of rows returned or 0 if nothing is returned
+    // int rowInserted = insertBob.executeUpdate();
+    // if(rowInserted > 0){
+    // System.out.println("A user has been inserted");
+    // }
+
+    // //Testing something else
+    // String userMarie = "INSERT INTO Users (username, password, fullname, email)
+    // VALUES (?, ?, ?, ?)";
+
+    // PreparedStatement insertMarie = conn.prepareStatement(userMarie);
+
+    // for(int i = 0; i < arr.length; i++){
+    // insertMarie.setString(i+1, arr[i]);
+    // }
+    // rowInserted = insertMarie.executeUpdate();
+    // if(rowInserted > 0){
+    // System.out.println("Marie has been added as a user!");
+    // }
+    // else {
+    // System.out.println("Insertion Failed");
+    // }
+    // } catch (SQLException ex) {
+    // ex.printStackTrace();
+    // }
+    // }
 }
